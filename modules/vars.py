92b32d2d5e6a044cc52096dd9d55c13a8da31af2
Prabhas <115615425+Prabhas505@users.noreply.github.com>
2024-04-14 09:10:47 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "29953693"
 api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7046735017:AAHwmcAyCex5gK2Oo83cY17Og9_8-I18B_A"
+bot_token = "6750231459:AAHt5t-sOt0xHW2GJ2UN1KDQxYnjyT8oxEU"
